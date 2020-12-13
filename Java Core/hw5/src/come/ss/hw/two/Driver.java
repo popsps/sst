2fcf7a5ae02f967acc378a51a9294d88af1a31eb
popsps <59066663+popsps@users.noreply.github.com>
2020-12-13 13:58:01 -0500

    ADD HW5 ASSIGNMENT 2 + COMMENTS

--- Java Core/hw5/src/come/ss/hw/two/Driver.java
+++ Java Core/hw5/src/come/ss/hw/two/Driver.java
@@ -2,6 +2,11 @@ package come.ss.hw.two;
 
 import java.util.Arrays;
 
+/**
+ * Demo
+ * get an array of non-negative Integers
+ * return an array of only the right-most digits
+ */
 @FunctionalInterface
 interface RightDigit {
   Integer[] apply(Integer[] numbers);
@@ -9,11 +14,14 @@ interface RightDigit {
 
 public class Driver {
   public static void main(String[] args) {
+    // define lambada
     RightDigit rightDigit = numbers -> {
       return Arrays.stream(numbers)
           .map(number -> number % 10).toArray(size -> new Integer[size]);
     };
-    Arrays.stream(rightDigit.apply(new Integer[]{1, 22, 93,4234,0,432}))
+    // print the result
+    // passing hard-coded example. Modify as you wish
+    Arrays.stream(rightDigit.apply(new Integer[]{1, 22, 93, 4234, 0, 432}))
         .forEach(number -> System.out.print(number + " "));
     System.out.println();
   }
