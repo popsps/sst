e7f82643d7b8d455f7728cd35a6d1857c15f4744
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2020-12-18 09:07:48 -0500

    Controller add more functionalities

--- Java Core/hw6_jdbc_demo/src/main/java/com/ss/hw/controller/CustomerController.java
+++ Java Core/hw6_jdbc_demo/src/main/java/com/ss/hw/controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.ss.hw.controller;
 import com.ss.hw.dao.CustomerDao;
 import com.ss.hw.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +16,15 @@ public class CustomerController {
     public CustomerController(CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
+
     @GetMapping("/")
     public String sayHello() {
         return "hello from Spring boot";
     }
-    @GetMapping("/api/customer")
-    public String findAllCustomer() {
-        return "hi";
+
+    @PostMapping("/api/customers")
+    public int addCustomer(@RequestBody Customer customer) {
+        return this.customerDao.save(customer);
     }
 
     @GetMapping("/api/customers")
