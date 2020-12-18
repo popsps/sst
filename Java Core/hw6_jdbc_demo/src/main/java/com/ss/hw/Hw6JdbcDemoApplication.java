f93116bee9765d5062fd4af28d0f60ad0b36f5a9
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2020-12-18 08:41:48 -0500

    Sync

---
+++ Java Core/hw6_jdbc_demo/src/main/java/com/ss/hw/Hw6JdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ss.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw6JdbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw6JdbcDemoApplication.class, args);
+	}
+
+}
---
+++ Java Core/hw6_jdbc_demo/src/test/java/com/ss/hw/Hw6JdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw6JdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
