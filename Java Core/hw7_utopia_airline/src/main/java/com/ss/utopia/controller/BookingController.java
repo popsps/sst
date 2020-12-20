27f7de033343cbacfcbcf78b82d67ff52bb9f6de
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2020-12-19 19:33:54 -0500

    layout BookingController

--- Java Core/hw7_utopia_airline/src/main/java/com/ss/utopia/controller/BookingController.java
+++ Java Core/hw7_utopia_airline/src/main/java/com/ss/utopia/controller/BookingController.java
@@ -1,9 +1,44 @@
 package com.ss.utopia.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ss.utopia.entity.Booking;
+import com.ss.utopia.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/bookings")
 public class BookingController {
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @PostMapping
+    public List<Booking> makeABooking() {
+        return bookingService.makeABooking();
+    }
+
+    @GetMapping("/{id}")
+    public Booking getMyBooking(@PathVariable("id") Long id) {
+        return bookingService.getMyBooking(id);
+    }
+
+    @PostMapping("/payment/{id}")
+    public Booking payForMyBooking(@PathVariable("id") Long id) {
+        return bookingService.payForMyBooking(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Booking deleteMyBooking(@PathVariable("id") Long id) {
+        return bookingService.deleteMyBooking(id);
+    }
 }
--- Java Core/hw7_utopia_airline/src/main/java/com/ss/utopia/service/BookingService.java
+++ Java Core/hw7_utopia_airline/src/main/java/com/ss/utopia/service/BookingService.java
@@ -1,8 +1,11 @@
 package com.ss.utopia.service;
 
 import com.ss.utopia.dao.BookingDao;
+import com.ss.utopia.entity.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
@@ -11,5 +14,21 @@ public class BookingService {
     @Autowired
     public BookingService(BookingDao bookingDao) {
         this.bookingDao = bookingDao;
+    }
+
+    public List<Booking> getAllBookings() {
+        return null;
+    }
+
+    public List<Booking> makeABooking() {
+    }
+
+    public Booking getMyBooking(Long id) {
+    }
+
+    public Booking payForMyBooking(Long id) {
+    }
+
+    public Booking deleteMyBooking(Long id) {
     }
 }
